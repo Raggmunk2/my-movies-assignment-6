61cc18b5f8f1d211ca69187d298a2ee95f20098c
Raggmunk2 <linnborgstrom93@gmail.com>
2023-05-08 18:06:10 +0200

    deleted the stupid pictured that did not work and added a url

--- my-movies/src/Movie.js
+++ my-movies/src/Movie.js
@@ -6,12 +6,12 @@ export default function Movie(movie){
     return (
         <ListGroupItem as="li" data-title={movie.title} data-grade={movie.grade}>
             {movie.title}
-            <img src="./images/delete.png" alt="Delete movie" className="delete-movie-icon"/>
+            <img src="https://png.pngtree.com/png-clipart/20191121/original/pngtree-3d-red-delete-icon-png-image_5142994.jpg" alt="Delete movie" className="delete-movie-icon"/>
             {getStars(movie.grade)}
         </ListGroupItem> 
     )
 }
 
 function getStars(numOfStars){
-    return "<img src='images/star.png' alt='star'".repeat(numOfStars);
+    return "<img src='https://png.pngtree.com/png-clipart/20220926/original/pngtree-shiny-gold-star-clipart-illustration-design-png-image_8633179.png' alt='star'".repeat(numOfStars);
 }
