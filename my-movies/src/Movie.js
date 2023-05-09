275ac818b182ea36ed953993d3dfa74599a61b71
Raggmunk2 <linnborgstrom93@gmail.com>
2023-05-09 19:03:26 +0200

    fixed the last with the star images

--- my-movies/src/Movie.js
+++ my-movies/src/Movie.js
@@ -4,22 +4,14 @@ import star from './/images/star.png'
 
 
 export default function Movie(props){
-    
+
     return (
         <li className="list-group-item" data-title={props.item.title} data-grade={props.item.grade}>
             {props.item.title}
             
             <img src={deleteBtn} alt="Delete movie" className="delete-movie-icon" onClick={() => props.deleteMovie(props.item.title)}/>
-            <img src="https://w7.pngwing.com/pngs/134/138/png-transparent-star-golden-stars-angle-3d-computer-graphics-symmetry-thumbnail.png" alt='star'/>
-            {`<img src='/images/star.png' alt='Star'/>`.repeat(props.item.grade)}
+           {Array.from({length: props.item.grade}, (_,i) => <img src={star} alt='star' key={i}/>)}
         </li> 
     )
         
-}
-
-function getStars(numOfStars){
-    
-    /* let starElement = `<img src='https://w7.pngwing.com/pngs/134/138/png-transparent-star-golden-stars-angle-3d-computer-graphics-symmetry-thumbnail.png' alt='star'/>`
-    return starElement.repeat(numOfStars); */
-    return "<img src='https://w7.pngwing.com/pngs/134/138/png-transparent-star-golden-stars-angle-3d-computer-graphics-symmetry-thumbnail.png' alt='Star'>".repeat(numOfStars);
 }
